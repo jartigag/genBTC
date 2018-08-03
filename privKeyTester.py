a4266fccdeff09614fae094f77f8e05d425e914d
jartigag <jartigag@protonmail.com>
2018-08-03 20:55:17 +0200

    wip2: privKeyTester

--- privKeyTester.py
+++ privKeyTester.py
@@ -7,37 +7,36 @@
 
 import argparse
 import sys
+import bitcoin
 
-def getWalletFromPassphrase(passphrase):
-	pass
+def getAddressFromPrivKey(privKey):
+	pubKey = bitcoin.fast_multiply(bitcoin.G, privKey)
+	address = bitcoin.pubkey_to_address(pubKey)
+	return address
 
 def getWalletInfosFromAddress(address):
 	pass
 
-def printWalletInfos(walletInfos, printOnlyWhenPositiveBalance, passphrase):
-	pass
-
-def explore(passphraseList, delay):
-	pass
+def printWalletInfos(walletInfos, printOnlyWhenPositiveBalance, privKey):
+    print("balance: %s\n btc. %d\n btc in %i total transactions"
+    	%(btcRemaining,btcReceived,nbTransactions))
 
 def testPrivKey(privKey):
-	pass
-
-def addPassphraseToFile(passphrase, filepath):
-	pass
-
-
+	address = getAddressFromPrivKey(privKey)
+	walletInfos = getWalletInfosFromAddress(address)
+	printWalletInfos(walletInfos, False, privKey)
 
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser(description=
 		"given a private key, look for its BTC transactions")
-	parser.add_argument('privKey',help="private key ")
+	parser.add_argument('privKey',help="private key (32-bytes number in hex)")
 
 	args = parser.parse_args()
 
 	if True: #TODO: if args.privKey is valid
-		privKey = args.privKey
+		str_privKey = args.privKey
+		privKey = int(str_privKey,16)
 	else:
 		print("invalid privKey")
 		sys.exit(0)
